54a4267d9557e625583f0842a9e255b29c99d998
윤수혁 <cooltime15@likelion.org>
2022-05-28 15:00:14 +0900

    샘플 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Button, TextField } from '@mui/material';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [input, setInput] = useState();
+
+  function setNumber(numbers){
+    setInput(input+numbers);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField value={input} id="standard-basic" variant="standard" />
+      <Button onClick={()=>{setNumber('1')}} variant="outlined" style={{width: '100px'}}>1</Button>
+      <Button onClick={()=>{setNumber('2')}} variant="outlined" style={{width: '100px'}}>2</Button>
+      <Button onClick={()=>{setNumber('3')}} variant="outlined" style={{width: '100px'}}>3</Button>
+
     </div>
   );
 }
